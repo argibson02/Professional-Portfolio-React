fc67a25e9eab674e74432034518d1dcd60496870
Alex Gibson <argibson02@gmail.com>
2021-11-22 19:54:52 -0800

    Added parallax effect to home background.

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Home.css';
 
 export default function Home() {
+  useEffect(() => {
+    // Add event listener
+    document.addEventListener("mousemove", parallax);
+    const elem = document.querySelector("#parallax");
+
+    // Magic happens here
+    function parallax(e) {
+      let _w = window.innerWidth / 2;
+      let _h = window.innerHeight / 2;
+      let _mouseX = e.clientX;
+      let _mouseY = e.clientY;
+      let _depth1 = `${50 - (_mouseX - _w) * 0.02}% ${50 - (_mouseY - _h) * 0.02}%`;
+      let _depth2 = `${50 - (_mouseX - _w) * 0.02}% ${50 - (_mouseY - _h) * 0.02}%`;
+      let _depth3 = `${50 - (_mouseX - _w) * 0.03}% ${50 - (_mouseY - _h) * 0.03}%`;
+      let x = `${_depth3}, ${_depth2}, ${_depth1}`;
+      // console.log(x);
+      elem.style.backgroundPosition = x;
+    }
+  });
+
   return (
     <div id="parallax" className="page">
       <section id="intro">
