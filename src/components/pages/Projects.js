945f1b9e0b017d79eab49e6740404a0ae2b44736
Alex Gibson <argibson02@gmail.com>
2021-11-23 22:08:59 -0800

    Added boilerplate styling to Porjects and Resume.

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,24 +1,51 @@
 import React from 'react';
+import './Projects.css';
+import './Footer.css';
+import Footer from './Footer.js';
+// import from './images/icons/.png'
 
 export default function Projects() {
   return (
-    <div>
-      <h1>Projects Page</h1>
-      <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-        dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-        sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-        sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-        vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-        libero. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-        lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-        in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-        bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-        est ligula suscipit velit, sed bibendum turpis dui in sapien.
-      </p>
+    <div className="page-bg vh-100">
+      <div className="container page-bg">
+        <div className="row">
+          <section className="h-100 bisection bisection-1 col-lg-6">
+            <h1 className="bisection-h1 bisection-1-h1">pdf</h1>
+            {/* <img className="profile-pic d-flex" src={headshot} alt="Professional headshot of me, Alex Gibson." /> */}
+            <p>
+              I am a full-stack developer with a strong history of software
+              quality assurance success and a desire to build products that work.
+              I am certified in full-stack development from UC Berkeley Extension
+              and have experience with Javascript, jQuery, HTML, CSS, React.js,
+              SQL, MongoDB, Node.js, Express.js, various web APIs, and more. I
+              have worn many hats in my previous roles as a software quality
+              assurance analyst and have participated in nearly every step of the
+              software development process. I bring a panoramic view to every project
+              I work on to ensure that the product developed is the product that
+              is needed. I am looking for new and exciting challenges as a full-stack
+              developer and hope to be a key member of your team!
+            </p>
+          </section>
+          <section className="h-100 bisection bisection-2 col-lg-6">
+            <h1 className="bisection-h1 bisection-2-h1">docx</h1>
+            {/* <img className="profile-pic d-flex" src={headshot} alt="Professional headshot of me, Alex Gibson." /> */}
+            <p>
+              I am a full-stack developer with a strong history of software
+              quality assurance success and a desire to build products that work.
+              I am certified in full-stack development from UC Berkeley Extension
+              and have experience with Javascript, jQuery, HTML, CSS, React.js,
+              SQL, MongoDB, Node.js, Express.js, various web APIs, and more. I
+              have worn many hats in my previous roles as a software quality
+              assurance analyst and have participated in nearly every step of the
+              software development process. I bring a panoramic view to every project
+              I work on to ensure that the product developed is the product that
+              is needed. I am looking for new and exciting challenges as a full-stack
+              developer and hope to be a key member of your team!
+            </p>
+          </section>
+        </div>
+        <Footer />
+      </div>
     </div>
   );
 }
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,22 +1,51 @@
 import React from 'react';
+import './Resume.css';
+import './Footer.css';
+import Footer from './Footer.js';
+// import from './images/icons/.png'
 
 export default function Resume() {
   return (
-    <div>
-      <h1>Resume Page</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
+    <div className="page-bg vh-100">
+      <div className="container page-bg">
+        <div className="row">
+          <section className="h-100 bisection bisection-1 col-lg-6">
+            <h1 className="bisection-h1 bisection-1-h1">pdf</h1>
+            {/* <img className="profile-pic d-flex" src={headshot} alt="Professional headshot of me, Alex Gibson." /> */}
+            <p>
+              I am a full-stack developer with a strong history of software
+              quality assurance success and a desire to build products that work.
+              I am certified in full-stack development from UC Berkeley Extension
+              and have experience with Javascript, jQuery, HTML, CSS, React.js,
+              SQL, MongoDB, Node.js, Express.js, various web APIs, and more. I
+              have worn many hats in my previous roles as a software quality
+              assurance analyst and have participated in nearly every step of the
+              software development process. I bring a panoramic view to every project
+              I work on to ensure that the product developed is the product that
+              is needed. I am looking for new and exciting challenges as a full-stack
+              developer and hope to be a key member of your team!
+            </p>
+          </section>
+          <section className="h-100 bisection bisection-2 col-lg-6">
+            <h1 className="bisection-h1 bisection-2-h1">docx</h1>
+            {/* <img className="profile-pic d-flex" src={headshot} alt="Professional headshot of me, Alex Gibson." /> */}
+            <p>
+              I am a full-stack developer with a strong history of software
+              quality assurance success and a desire to build products that work.
+              I am certified in full-stack development from UC Berkeley Extension
+              and have experience with Javascript, jQuery, HTML, CSS, React.js,
+              SQL, MongoDB, Node.js, Express.js, various web APIs, and more. I
+              have worn many hats in my previous roles as a software quality
+              assurance analyst and have participated in nearly every step of the
+              software development process. I bring a panoramic view to every project
+              I work on to ensure that the product developed is the product that
+              is needed. I am looking for new and exciting challenges as a full-stack
+              developer and hope to be a key member of your team!
+            </p>
+          </section>
+        </div>
+        <Footer />
+      </div>
     </div>
   );
 }
