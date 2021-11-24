8518a7004d70a535b8a24018884702930a87acd9
Alex Gibson <argibson02@gmail.com>
2021-11-23 23:38:21 -0800

    Added links to the project buttons

--- src/components/pages/ProjectsList.js
+++ src/components/pages/ProjectsList.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import './Projects.css';
-import './Footer.css';
+import './ProjectsList.css';
 
 // import from './images/icons/.png'
 
@@ -8,14 +7,15 @@ export default function ProjectList(props) {
   return (
     <div className="col-md-4">
       <div className="shadow card-style card">
-        <div className="card-img" variant="top" src={props.project.picUrl} />
+        {/* <div className="card-img" variant="top" src={props.project.picUrl} /> */}
         <div className="card-body" >
+          <img src={props.project.picUrl} alt="projectImage" className="rounded-circle" width="140" height="140" preserveAspectRatio="xMidYMid slice" focusable="false" />
           <div className="card-title" ><strong>{props.project.title}</strong></div>
           <div className="card-text">{props.project.description}</div>
           <div className="center">
             {/* swap out these button color schemes later */}
-            <button className="btn btn-secondary mx-1"  >Demo</button>
-            <button className="btn btn-secondary mx-1"  >Git</button>
+            <button className="btn btn-secondary mx-1"> <a href={props.project.appUrl} className="project-link" target="_blank" rel="noreferrer">Demo</a></button>
+            <button className="btn btn-secondary mx-1"> <a href={props.project.gitUrl} className="project-link" target="_blank" rel="noreferrer">Git</a></button>
           </div>
         </div>
       </div>
