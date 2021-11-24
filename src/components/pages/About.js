4094156b8bee73e9a4f891093eb680b3b9cc19ec
Alex Gibson <argibson02@gmail.com>
2021-11-23 21:17:29 -0800

    Added validation for input form on the contact page. General CSS improvements on the contact page.

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -16,41 +16,43 @@ import react from './images/icons/react.png'
 
 export default function About() {
   return (
-    <div className="container">
-      <div className="row">
-        <section className="h-100 bisection bisection-1 col-lg-6">
-          <h1 className="bisection-h1 bisection-1-h1">about me</h1>
-          <img className="profile-pic d-flex" src={headshot} alt="Professional headshot of me, Alex Gibson." />
-          <p>
-            I am a full-stack developer with a strong history of software
-            quality assurance success and a desire to build products that work.
-            I am certified in full-stack development from UC Berkeley Extension
-            and have experience with Javascript, jQuery, HTML, CSS, React.js,
-            SQL, MongoDB, Node.js, Express.js, various web APIs, and more. I
-            have worn many hats in my previous roles as a software quality
-            assurance analyst and have participated in nearly every step of the
-            software development process. I bring a panoramic view to every project
-            I work on to ensure that the product developed is the product that
-            is needed. I am looking for new and exciting challenges as a full-stack
-            developer and hope to be a key member of your team!
-          </p>
-        </section>
-        <section className="h-100 bisection bisection-2 col-lg-6">
-          <h1 className="bisection-h1 bisection-2-h1">skills</h1>
-          <ul className="skills-ul">
-            <li className="skills-li"><img src={css} className="skills-img col-6" type="image" alt="css" /><span className="skills-label col-6"> CSS</span></li>
-            <li className="skills-li"><img src={express} className="skills-img col-6" type="image" alt="express" /><span className="skills-label col-6"> express.js</span></li>
-            <li className="skills-li"><img src={handlebars} className="skills-img col-6" type="image" alt="handlebars" /><span className="skills-label col-6"> Handlebars.js</span></li>
-            <li className="skills-li"><img src={html} className="skills-img col-6" type="image" alt="html" /><span className="skills-label col-6"> HTML</span></li>
-            <li className="skills-li"><img src={js} className="skills-img col-6" type="image" alt="javascript" /><span className="skills-label col-6"> JavaScript</span></li>
-            <li className="skills-li"><img src={mongodb} className="skills-img col-6" type="image" alt="mongodb" /><span className="skills-label col-6"> MongoDB</span></li>
-            <li className="skills-li"><img src={mysql} className="skills-img col-6" type="image" alt="mysql" /><span className="skills-label col-6"> MySQL</span></li>
-            <li className="skills-li"><img src={nodejs} className="skills-img col-6" type="image" alt="nodejs" /><span className="skills-label col-6"> Node.js</span></li>
-            <li className="skills-li"><img src={react} className="skills-img col-6" type="image" alt="react" /><span className="skills-label col-6"> React</span></li>
-          </ul>
-        </section>
+    <div className="page-bg">
+      <div className="container page-bg">
+        <div className="row">
+          <section className="h-100 bisection bisection-1 col-lg-6">
+            <h1 className="bisection-h1 bisection-1-h1">about me</h1>
+            <img className="profile-pic d-flex" src={headshot} alt="Professional headshot of me, Alex Gibson." />
+            <p>
+              I am a full-stack developer with a strong history of software
+              quality assurance success and a desire to build products that work.
+              I am certified in full-stack development from UC Berkeley Extension
+              and have experience with Javascript, jQuery, HTML, CSS, React.js,
+              SQL, MongoDB, Node.js, Express.js, various web APIs, and more. I
+              have worn many hats in my previous roles as a software quality
+              assurance analyst and have participated in nearly every step of the
+              software development process. I bring a panoramic view to every project
+              I work on to ensure that the product developed is the product that
+              is needed. I am looking for new and exciting challenges as a full-stack
+              developer and hope to be a key member of your team!
+            </p>
+          </section>
+          <section className="h-100 bisection bisection-2 col-lg-6">
+            <h1 className="bisection-h1 bisection-2-h1">skills</h1>
+            <ul className="skills-ul">
+              <li className="skills-li"><img src={css} className="skills-img col-6" type="image" alt="css" /><span className="skills-label col-6"> CSS</span></li>
+              <li className="skills-li"><img src={express} className="skills-img col-6" type="image" alt="express" /><span className="skills-label col-6"> express.js</span></li>
+              <li className="skills-li"><img src={handlebars} className="skills-img col-6" type="image" alt="handlebars" /><span className="skills-label col-6"> Handlebars.js</span></li>
+              <li className="skills-li"><img src={html} className="skills-img col-6" type="image" alt="html" /><span className="skills-label col-6"> HTML</span></li>
+              <li className="skills-li"><img src={js} className="skills-img col-6" type="image" alt="javascript" /><span className="skills-label col-6"> JavaScript</span></li>
+              <li className="skills-li"><img src={mongodb} className="skills-img col-6" type="image" alt="mongodb" /><span className="skills-label col-6"> MongoDB</span></li>
+              <li className="skills-li"><img src={mysql} className="skills-img col-6" type="image" alt="mysql" /><span className="skills-label col-6"> MySQL</span></li>
+              <li className="skills-li"><img src={nodejs} className="skills-img col-6" type="image" alt="nodejs" /><span className="skills-label col-6"> Node.js</span></li>
+              <li className="skills-li"><img src={react} className="skills-img col-6" type="image" alt="react" /><span className="skills-label col-6"> React</span></li>
+            </ul>
+          </section>
+        </div>
+        <Footer />
       </div>
-      <Footer />
     </div>
   );
 }
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,22 +1,129 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Contact.css';
+import './Footer.css';
+import Footer from './Footer.js';
+import css from './images/icons/css.png'
+import express from './images/icons/express.png'
+import handlebars from './images/icons/handlebars.png'
+import html from './images/icons/html.png'
+import js from './images/icons/js.png'
+import mongodb from './images/icons/mongodb.png'
+import mysql from './images/icons/mysql.png'
+import nodejs from './images/icons/nodejs.png'
+import react from './images/icons/react.png'
 
-export default function Contact() {
+import { validateEmail } from './utils/helpers';
+// import from './images/icons/.png'
+
+
+function Contact() {
+
+
+  // remove  //
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [contactName, setContactName] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleInputChange = (e) => {
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    if (inputType === 'email') {
+      setEmail(inputValue);
+    } else if (inputType === 'contactName') {
+      setContactName(inputValue);
+    } else {
+      setMessage(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (!validateEmail(email)) {
+      setErrorMessage('Email is invalid');
+      return;
+    }
+
+    setContactName('');
+    setMessage('');
+    setEmail('');
+  };
+  // remove //
+
+
+
+
   return (
-    <div>
-      <h1>Contact Page</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
+    <div className="page-bg">
+      <div className="container page-bg">
+        <div className="row">
+          <section className="h-100 bisection bisection-4 col-lg-6">
+            <h1 className="bisection-h1 bisection-4-h1">contact me</h1>
+            <ul className="skills-ul">
+              <li className="skills-li"><img src={css} className="skills-img col-6" type="image" alt="css" /><span className="skills-label col-6"> CSS</span></li>
+              <li className="skills-li"><img src={express} className="skills-img col-6" type="image" alt="express" /><span className="skills-label col-6"> express.js</span></li>
+              <li className="skills-li"><img src={handlebars} className="skills-img col-6" type="image" alt="handlebars" /><span className="skills-label col-6"> Handlebars.js</span></li>
+              <li className="skills-li"><img src={html} className="skills-img col-6" type="image" alt="html" /><span className="skills-label col-6"> HTML</span></li>
+              <li className="skills-li"><img src={js} className="skills-img col-6" type="image" alt="javascript" /><span className="skills-label col-6"> JavaScript</span></li>
+              <li className="skills-li"><img src={mongodb} className="skills-img col-6" type="image" alt="mongodb" /><span className="skills-label col-6"> MongoDB</span></li>
+              <li className="skills-li"><img src={mysql} className="skills-img col-6" type="image" alt="mysql" /><span className="skills-label col-6"> MySQL</span></li>
+              <li className="skills-li"><img src={nodejs} className="skills-img col-6" type="image" alt="nodejs" /><span className="skills-label col-6"> Node.js</span></li>
+              <li className="skills-li"><img src={react} className="skills-img col-6" type="image" alt="react" /><span className="skills-label col-6"> React</span></li>
+            </ul>
+          </section>
+          <section className="h-100 bisection bisection-3 col-lg-6">
+            <h1 className="bisection-h1 bisection-3-h1">contact form</h1>
+            <form>
+              <div className="form-group mb-2">
+                <label for="email">Email address</label>
+                <input
+                  value={email}
+                  name="email"
+                  onChange={handleInputChange}
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  aria-describedby="emailHelp"
+                  placeholder="Enter email" />
+              </div>
+              <div className="form-group mb-2">
+                <label for="contact-name">Name</label>
+                <input
+                  value={contactName}
+                  name="contactName"
+                  onChange={handleInputChange}
+                  type="contactName"
+                  className="form-control"
+                  id="contact-name"
+                  placeholder="Sammy Sample" />
+              </div>
+              <div className="form-group mb-2">
+                <label for="message">Message</label>
+                <textarea
+                  value={message}
+                  name="message"
+                  onChange={handleInputChange}
+                  type="message"
+                  className="form-control"
+                  id="message"
+                  rows="3"></textarea>
+              </div>
+              <button type="submit" className="btn btn-primary" onClick={handleFormSubmit}>Submit</button>
+            </form>
+            {errorMessage && (
+              <div>
+                <p className="error-text">{errorMessage}</p>
+              </div>
+            )}
+          </section>
+        </div>
+        <Footer />
+      </div>
     </div>
   );
 }
+
+export default Contact;
